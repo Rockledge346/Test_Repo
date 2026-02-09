3724c5ff72b8c7b2d7f0a60b47c1f1986027d493
Fionn <rockledge346@proton.me>
2026-02-09 13:43:59 +0000

    Added HelloATU.java

---
+++ HelloATU.java
@@ -0,0 +1,11 @@
+public class HelloATU {
+	
+	public static void main(String[] args) {
+	
+	//Prints "Hello, ATU from Fionn!" as output string
+	
+	System.out.println("Hello, ATU from Fionn!");
+	
+	}
+	
+}
